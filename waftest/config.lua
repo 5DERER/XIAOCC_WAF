90779c62558b280999b2091ef7eadcedd18b6865
5DERER <114556019+5DERER@users.noreply.github.com>
2024-03-03 18:27:21 +0800

    Update config.lua

--- waftest/config.lua
+++ waftest/config.lua
@@ -1,35 +1,35 @@
 --WAF config file,enable = "on",disable = "off"
 
---waf status
+--waf I/O
 config_waf_enable = "on"
---log dir
+--log dir_path
 config_log_dir = "/tmp"
---rule setting
+--rule path
 config_rule_dir = "/usr/local/openresty/nginx/conf/waftest/rule"
---enable/disable white url
+--white url
 config_white_url_check = "on"
---enable/disable white ip
+--white ip
 config_white_ip_check = "on"
---enable/disable block ip
+--black ip
 config_black_ip_check = "on"
---enable/disable url filtering
+--url
 config_url_check = "on"
---enalbe/disable url args filtering
+--url args
 config_url_args_check = "on"
---enable/disable user agent filtering
+--user agent
 config_user_agent_check = "on"
---enable/disable cookie deny filtering
+--cookie
 config_cookie_check = "on"
---enable/disable cc filtering
+--cc
 config_cc_check = "on"
---cc rate the xxx of xxx seconds
+--cc rate
 config_cc_rate = "10/60"
---enable/disable post filtering
+--post
 config_post_check = "on"
---config waf output redirect/html
+--waf output
 config_waf_output = "html"
---if config_waf_output ,setting url
-config_waf_redirect_url = "https://www.baidu.com"
+--redirect html
+config_waf_redirect_url = "https://8.130.139.21/a"
 config_output_html=[[
 <html>
 <head>
@@ -38,7 +38,7 @@ config_output_html=[[
 <title>Web应用防火墙</title>
 </head>
 <body>
-<h1 align="center"> 防护成功
+<h1 align="center"> 禁止未经允许的渗透测试行为，你的行为已经被记录，我司保留报案起诉权力！
 </body>
 </html>
 ]]
